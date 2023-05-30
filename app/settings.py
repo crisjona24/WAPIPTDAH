2a20afa96080c3b039add128598ceec6c4546559
Cristobal <cristobal.rios@unl.edu.ec>
2023-05-30 11:51:27 -0500

    Cambio en settings

--- app/settings.py
+++ app/settings.py
@@ -53,10 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'app.urls'
+
 
 TEMPLATES = [
     {
